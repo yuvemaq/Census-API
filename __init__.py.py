7377d22c4ed045620093596b20b4d34d722d32ee
yuvemaq <78762744+yuvemaq@users.noreply.github.com>
2021-09-15 10:03:25 -0400

    __version__

---
+++ __init__.py.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from census.core import (Census, ALL, CensusException,
+                         UnsupportedYearException)
+
+
+# In[ ]:
+
+
+
+
